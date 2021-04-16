7f5b1122a9050beb0c5f327cac3f1e51b48b4b72
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2021-04-16 10:50:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,7 @@ option=st.sidebar.selectbox("options",("Choose one of the options below","GL Out
 linkedin='<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="thierry-haddad" data-version="v1" ><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/thierry-haddad?trk=profile-badge" target="_blank">Click here</a></div>'
 
 if option=="Choose one of the options below":
+    write()
     st.write("Click here to launch my Linkedin page")
     pic=Image.open('linkedin.png')
     st.image(pic)
