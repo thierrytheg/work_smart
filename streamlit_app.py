b1bed073748dca52718cd92a289cbc35d5eb0aec
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2021-04-16 10:36:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def download_link_csv(object_to_download, download_filename, download_link_text)
 
 option=st.sidebar.selectbox("options",("Choose one of the options below","GL Outliers","Intercompany"))
 
-linkedin='<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="thierry-haddad" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/thierry-haddad?trk=profile-badge" target="_blank"></a></div>'
+linkedin='<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="thierry-haddad" data-version="v1" ><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/thierry-haddad?trk=profile-badge" target="_blank">Click here</a></div>'
 
 if option=="Choose one of the options below":
     components.html(linkedin,height=1000)
