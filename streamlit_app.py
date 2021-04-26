6ff27e0d817bbc682936a6da39157b22b220d7f7
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2021-04-26 11:05:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def generate_chord(df):
     names=list(df_pivot.columns)
    
     url = "https://api.shahin.dev/chord"
-    payload={'names': names, 'matrix':matrix,'width': 900,'verb':'','conjuction':'','noun':'','padding':0.1,'symmetric':False,'asymmetric':False,'allow_download':True}
+    payload={'names': names, 'matrix':matrix,'width': 800,'verb':'','conjuction':'','noun':'','padding':0.1,'symmetric':False,'asymmetric':False,'allow_download':True}
   
 
     user=st.secrets["user"]
