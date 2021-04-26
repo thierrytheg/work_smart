fd26d13b1edf0f44926e18f549695eae14162504
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2021-04-26 12:01:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ if option=="Intercompany":
 
                 except Exception as e:
                     
-                    st.error("%s/n%s") %(e,error1)
+                    st.error(%s/n%s) %(e,error1)
                     st.stop()
 
         else:
@@ -108,7 +108,7 @@ if option=="Intercompany":
 
     except Exception as e:
               
-        st.error("%s/n%s") %(e,error1)
+        st.error(%s/n%s) %(e,error1)
         st.stop()
     
     generate_chord(df)  
