2b53f98537631c4ebd6f46242bd21250bb1f1a72
thierrytheg <32854618+thierrytheg@users.noreply.github.com>
2021-04-26 17:02:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ error1='Make sure to use the template to avoid any errors.'
 error2='If the problem persists, do not hesitate to contact me.'
 
 def generate_chord(df):
+    df['Amount']=(df['Amount']).astype(str)
+    
     df_pivot=df.pivot_table(values='Amount',
                         index=['Company From'],
                         columns=['Company To'],
